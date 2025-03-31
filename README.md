563b4d7b70b6b61ffd029e01ff43434c67f11dd2
Ekaterina Urbakh <ridiculousprince@gmail.com>
2025-03-31 14:43:11 +0500

    commit1

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# b1
+# profi_serv_a
+
+### Installation
+```
+cd ~/catkin_ws/src/
+git clone https://github.com/voltbro/profi_serv_a.git
+```
+### Compilation
+```
+cd ~/catkin_ws/
+catkin_make --pkg profi_serv_a
+```
+### Usage
+
+Just run configure.launch
+```
+roslaunch profi_serv_a start_configure_a.launch
+```
+
+### Required libraries
+
+```
+string
+time
+os
+tqdm
+os
+random
+```
---
+++ src/service_configuration.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+import string
+import time
+import os
+import random
+
+configuration_number = "1xM18650"
+version = "1.0.0"
+
+print(os.system("ls -la"))
+
+def print_random_str(l):
+    for i in range(l):
+        char = chr(random.randint(32, 126))
+        print(char, end='', flush=True)
+        time.sleep(0.03)
+    
+    return (' ')
+
+print(print_random_str(40))
+print(print_random_str(40))
+print(print_random_str(40))
+print(print_random_str(40))
+print(print_random_str(40))
+
+print("Service package 1: ver. {}".format(version))
+time.sleep(0.5)
+print("Service package 1: start configuration ")
+time.sleep(0.5)
+
+for i in tqdm.tqdm(range(3000), ascii=True, desc="System check"):
+    time.sleep(0.001)
+
+print("")
+print(os.system("ifconfig | grep -w inet"))
+
+print("Service package 1: successfully configured!")
+time.sleep(0.5)
+print("Service package 1: configuration checksum : {}".format(configuration_number))
+print("Service package 1: please push Ctrl+C to finish")
